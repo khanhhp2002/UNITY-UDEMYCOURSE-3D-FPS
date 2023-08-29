975ed86376c4f6214723e1d71def3afa1d7a0e2e
khanhbq_he2002 <khanhbui1560@gmail.com>
2023-08-29 22:12:21 +0700

    update 2

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    int a = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // afffsf
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        a += 1;
     }
 }
